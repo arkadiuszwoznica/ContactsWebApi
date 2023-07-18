d7892bbae0799d1087058536595c0268a79dfd72
arkadiuszwoznica <arkadiusz.piotr.woznica@gmail.com>
2023-07-18 12:57:40 +0200

    firstContactsHardcoded

---
+++ Contacts/Controllers/ContactsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+namespace Contacts.Controllers
+{
+	[ApiController]
+	public class ContactsController : Controller
+	{
+		[HttpGet("api/contacts")]
+		public IActionResult Get()
+		{
+			return new JsonResult(
+				new List<object>()
+				{
+					new {Id =1, FirstName = "Jan", LastName = "Kowalski", Email="jkowalski@w.pl"},
+					new {Id =2, FirstName = "Julian", LastName = "Miron", Email="jmiron@w.pl"}
+				}
+			);
+		}
+	}
+}
