b2486c833c947bbc12518e7d549a05da55d6e878
arkadiuszwoznica <arkadiusz.piotr.woznica@gmail.com>
2023-07-18 13:38:37 +0200

    domain and dto

---
+++ Contacts/DTOs/ContactDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Contacts.DTOs
+{
+	public class ContactDto
+	{
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string FullName => $"{FirstName} {LastName}"; 
+        public string Email { get; set; } = string.Empty;
+    }
+}
