d0b65d01bb8e2f971e48a7a277db50235f440864
arkadiuszwoznica <arkadiusz.piotr.woznica@gmail.com>
2023-07-18 13:47:43 +0200

    singleton pattern

--- Contacts/Controllers/ContactsController.cs
+++ Contacts/Controllers/ContactsController.cs
@@ -1,20 +1,18 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
+using Contacts.Infrastructure;
 namespace Contacts.Controllers
 {
 	[ApiController]
 	[Route("api/contacts")]
 	public class ContactsController : ControllerBase
 	{
+
 		[HttpGet]
 		public IActionResult Get()
 		{
 			return new JsonResult(
-				new List<object>()
-				{
-					new {Id =1, FirstName = "Jan", LastName = "Kowalski", Email="jkowalski@w.pl"},
-					new {Id =2, FirstName = "Julian", LastName = "Miron", Email="jmiron@w.pl"}
-				}
+				DataService.Instance.Contacts
 			);
 		}
 	}
---
+++ Contacts/Infrastructure/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Contacts.Domain;
+namespace Contacts.Infrastructure
+{
+	public class DataService
+	{
+		public List<Contact> Contacts { get; }
+
+		public static DataService Instance { get; } = new DataService();
+
+		private DataService()
+		{
+			Contacts = new List<Contact>()
+			{
+					new Contact() {Id =1, FirstName = "Jan", LastName = "Kowalski", Email="jkowalski@w.pl"},
+					new Contact() {Id =2, FirstName = "Julian", LastName = "Miron", Email="jmiron@w.pl"}
+			};
+		}
+	}
+}
