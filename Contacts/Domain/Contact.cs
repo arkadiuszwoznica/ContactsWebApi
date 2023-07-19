4b0367e9b3d81db9a41ad6bd11278fc405013afd
arkadiuszwoznica <arkadiusz.piotr.woznica@gmail.com>
2023-07-19 20:49:26 +0200

    classes prepared for entity

--- Contacts/Domain/Contact.cs
+++ Contacts/Domain/Contact.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Contacts.Domain
 {
 	public class Contact
 	{
+        [Key]
 		public int Id { get; set; }
-		public string FirstName { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(32)]
+        public string FirstName { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(64)]
         public string LastName { get; set; } = string.Empty;
+        [MaxLength(128)]
         public string Email { get; set; } = string.Empty;
-		public ICollection<Phone> Phones { get; set; } = new List<Phone>();
+		public List<Phone> Phones { get; set; } = new ();
     }
 }
