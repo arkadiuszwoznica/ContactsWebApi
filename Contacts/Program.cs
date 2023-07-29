57929b5e9073bd97f80e7c362c52be5fe842d30f
Arkadiusz Woźnica <115310756+arkadiuszwoznica@users.noreply.github.com>
2023-07-29 11:41:38 +0200

    cache base

--- Contacts/Program.cs
+++ Contacts/Program.cs
@@ -1,31 +1,60 @@
 ﻿using System.Reflection;
 using Contacts.Infrastructure;
 using Contacts.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddDbContext<ContactsDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("ContactsDb"));
     options.EnableSensitiveDataLogging(builder.Environment.IsDevelopment());
 });
 
-builder.Services.AddControllers().AddNewtonsoftJson(options =>
-{
-    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-});
-
 builder.Services.AddScoped<IContactsRepository, ContactsRepository>();
 
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policyBuilder =>
+    {
+        policyBuilder
+            .WithOrigins("http://localhost:3000", "http://localhost:5173")
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddControllers(configure =>
+{
+    configure.CacheProfiles.Add("Any-60",
+        new Microsoft.AspNetCore.Mvc.CacheProfile
+        {
+            NoStore = true
+        });
+    configure.CacheProfiles.Add("NoCache",
+    new Microsoft.AspNetCore.Mvc.CacheProfile
+    {
+        Location = ResponseCacheLocation.Any,
+        Duration = 60
+    });
+}).AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddResponseCaching();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -38,7 +67,10 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseCors();
+
+app.UseResponseCaching();
+
 app.MapControllers();
 
 app.Run();
-
